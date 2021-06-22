00d4e5371cfba4d79482db98d14fa43fdab446f7
sabin-nepal <sabinnepal2k17@gmail.com>
2021-06-22 17:12:30 +0545

    category api url

--- lib/app/core/url.dart
+++ lib/app/core/url.dart
@@ -4,7 +4,7 @@ class UrlHelper{
   static const String newsTypeUrl = '${host}news/type';
   static const String newsSearchUrl = '${host}news/search';
   static const String insightUrl = '${host}insight';
-  static const String categoryUrl = '${host}category';
+  static const String categoryUrl = '${host}category/all/-1';
   static const String categoryNewsUrl = '${host}category/news';
   static const String newsCategoryUrl = '${host}news/category';
   static const String videoNewsUrl = '${host}video';
